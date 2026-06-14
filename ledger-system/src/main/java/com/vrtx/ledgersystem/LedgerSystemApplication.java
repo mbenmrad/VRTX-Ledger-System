93a8b22b9353151b48551ddab3e16df04d719f99
mbenmrad <med.benmrad90@gmail.com>
2026-06-14 16:37:04 +0200

    Initial Spring Boot skeleton: project setup, Docker, DB connection, README

---
+++ ledger-system/src/main/java/com/vrtx/ledgersystem/LedgerSystemApplication.java
@@ -0,0 +1,12 @@
+package com.vrtx.ledgersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgerSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LedgerSystemApplication.class, args);
+    }
+}
---
+++ ledger-system/src/test/java/com/vrtx/ledgersystem/LedgerSystemApplicationTests.java
@@ -0,0 +1,12 @@
+package com.vrtx.ledgersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
